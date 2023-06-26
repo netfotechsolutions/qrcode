db04f0aeb95822ec177c5e4205393e9abc23ac78
Tushar Patil <tj4884@gmail.com>
2023-06-26 11:40:42 +0530

    UI changes

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,16 +1,15 @@
-import { WaveLoader  } from "react-loaders-kit";
+import { DotsLoader } from "react-loaders-kit";
 export const Loading = () => {
   const loaderProps = {
     loading: true,
     size: 45,
-    duration: 0.6,
+    duration: 1.5,
     colors: ['#0078d4']
   };
   return (
     <>
     <div className="center-screen">
-    <WaveLoader  {...loaderProps} />
-    <div style={{marginTop:"90px", marginLeft:"-63px"}}>Please wait...</div>
+    <DotsLoader  {...loaderProps} />
     </div>
     </>
   );
--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -17,7 +17,7 @@ export const Header = (props) => {
             className="border-noround bg-color pl-4 pr-4 mt-6 lg:mt-8 md:mt-8"
             value={props.badgeValue}
           ></Badge>
-          <div className="gothic_bold_condensed text-white text-5xl font-semibold mt-4 md:mt-4 lg:mt-6">
+          <div className="gothic_bold_condensed text-white text-3xl lg:text-5xl md:text-5xl font-semibold mt-4 md:mt-4 lg:mt-6">
             {" "}
             {props.title}
           </div>
